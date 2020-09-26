b751bd18f6e3e0be458db6fce8bbedca69f7b77d
bong <devbonga@gmail.com>
2020-09-27 00:37:10 +0900

    #9 : 가격 추출 부분 리팩토링 - 가격이 없거나 누락된 경우 RuntimeException 발생

--- src/main/java/net/devpg/rpinder/batch/collector/extractor/DailyGoldPriceExtractor.java
+++ src/main/java/net/devpg/rpinder/batch/collector/extractor/DailyGoldPriceExtractor.java
@@ -1,10 +1,12 @@
 package net.devpg.rpinder.batch.collector.extractor;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.util.Strings;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import net.devpg.rpinder.batch.collector.CrawlingPattern;
-import net.devpg.rpinder.batch.collector.extractor.PriceExtractor;
 import net.devpg.rpinder.batch.collector.vo.GoldPrice;
 import net.devpg.rpinder.batch.collector.vo.GoldPriceByType;
 import net.devpg.rpinder.batch.collector.vo.Price;
@@ -27,6 +28,7 @@ public class DailyGoldPriceExtractor implements PriceExtractor<Document, GoldPri
 
     @Override
     public GoldPrice extract(Document document) {
+        log.info(document.toString());
         //Common Pattern
         Elements commonElement = document.select(crawlingPattern.common());
 
@@ -39,22 +41,26 @@ public class DailyGoldPriceExtractor implements PriceExtractor<Document, GoldPri
     }
 
     private GoldPriceByType extractGoldPriceBySell(Elements commonElement) {
-        Elements sellCommonElement = commonElement.select(crawlingPattern.sell().common());
+        final BigDecimal MINUS_ONE = BigDecimal.valueOf(-1L);
         List<Price> goldPriceList = Optional.ofNullable(crawlingPattern.sell().golds())
             .orElse(Collections.emptyList())
             .stream()
-            .map(pattern -> {
-                Elements patternElement = sellCommonElement.select(pattern);
-                return Price.from(BigDecimalUtil.valueOf(patternElement.text().replace(",", "")));
-            })
+            .map(pattern -> Optional.ofNullable(commonElement.select(pattern))
+                .filter(elements -> !Strings.isBlank(elements.text()))
+                .map(elements -> Price.from(BigDecimalUtil.valueOf(elements.text().replace(",", ""))))
+                .orElse(Price.from(MINUS_ONE)))
             .collect(Collectors.toList());
-        return goldPriceList.isEmpty() ? null :
-            GoldPriceByType.of(goldPriceList.get(0), goldPriceList.get(1), goldPriceList.get(2));
+        if (goldPriceList.isEmpty() || goldPriceList.stream()
+            .anyMatch(price -> price.value().compareTo(MINUS_ONE) == 0))
+            throw new RuntimeException("Part of the selling price does not exist");
+        return GoldPriceByType.of(goldPriceList.get(0), goldPriceList.get(1), goldPriceList.get(2));
     }
 
     private GoldPriceByType extractGoldPriceByPurchase(Elements commonElement) {
-        Elements elements = commonElement.select(crawlingPattern.purchase().gold24k());
-        return GoldPriceByType.of(
-            Price.from(BigDecimalUtil.valueOf(elements.text().replace(",", ""))), null, null);
+        return Optional.of(commonElement.select(crawlingPattern.purchase().gold24k()))
+            .filter(elements -> !Strings.isBlank(elements.text()))
+            .map(elements -> GoldPriceByType.of(
+                Price.from(BigDecimalUtil.valueOf(elements.text().replace(",", ""))), null, null))
+            .orElseThrow(() -> new RuntimeException("The purchase price does not exist"));
     }
 }
